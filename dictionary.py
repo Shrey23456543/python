2bca83d1907f9bb374d9969608685a18775ca690
Shrey23456543 <sheyashkhairnar@gmail.com>
2025-05-05 18:40:36 +0530

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,10 @@
+#users={"name":"Alice","email":"Alice@gmail.com","active":True},
+#{"name":"Shreyash","email":"shrey@gmail.com","active":False}
+
+#active_email=[x['email']for x in users if x['active']]
+#print(active_email)
+
+person={"name":"shreyash","age":21,"Sex":"male"}
+fset=frozenset(person)#frozen set are immutable
+print(fset)
+    
